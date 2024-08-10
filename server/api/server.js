87f35c4afb6d88454f9c5e46d4d5a49bcc43b54e
DennisRono <dennisrkibet@gmail.com>
2024-08-10 17:48:00 +0300

    funsctions with builds conflicts

--- server/api/server.js
+++ server/api/server.js
@@ -1,17 +1,31 @@
-import { create, router as _router, defaults, rewriter } from 'json-server'
-const server = create()
-const router = _router('db.json')
-const middlewares = defaults()
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+
+const server = jsonServer.create()
+
+// Uncomment to allow write operations
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
+
+// Comment out to allow write operations
+const router = jsonServer.router('db.json')
+
+const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-server.use(
-  rewriter({
-    '/*': '/',
-  })
-)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
 server.use(router)
 server.listen(3000, () => {
-  console.log('JSON Server is running')
+    console.log('JSON Server is running')
 })
 
-export default server
+// Export the Server API
+module.exports = server
